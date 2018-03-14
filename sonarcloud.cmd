9483d80912da07ea4efc8967e7ad299c247b4ad7
Eric Kerst <eric.kerst@gmail.com>
2018-03-15 00:29:26 +0100

    Typooo

--- sonarcloud.cmd
+++ sonarcloud.cmd
@@ -4,16 +4,23 @@ REM Change ScannerLocation to where SonarQube.Scanner.MSBuild.exe is located
 REM Open a visual studio developer command prompt!
 REM Change path to where SOLID.sln is located (eg. D:\Projects\SOLID)
 
+REM Set execution directory of scanner utility
 SET ScannerLocation=D:\SonarQube\Scanner
 
+REM Remove previous testresults and coverage
 rd /s /q "%CD%\TestResults"
 md "%CD%\TestResults"
 
-%ScannerLocation%\SonarQube.Scanner.MSBuild.exe begin /k:"SOLID" /d:sonar.organization="k7o-github" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.login="e23d98e90e5f0bcea19b5278753638d6cdb83eab" /d:sonar.cs.vstest.reportsPaths="%CD%\TestResults\*.trx" /d:sonar.cs.vscoveragexml.reportsPaths="%CD%\TestResults\*.coveragexml"
+REM Start analyzer
+%ScannerLocation%\SonarQube.Scanner.MSBuild.exe begin /k:"SOLID" /d:sonar.organization="k7o-github" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.login="de76097ef248e051959c31eb08f27898c306f311" /d:sonar.cs.vstest.reportsPaths="%CD%\TestResults\*.trx" /d:sonar.cs.vscoveragexml.reportsPaths="%CD%\TestResults\*.coveragexml"
 
+REM (re)Build solution
 MsBuild.exe /t:Rebuild SOLID.sln
 
+REM Execute tests and collect coverage with "Dynamic Code Coverage Tools\CodeCoverage.exe" (hint: read statement from right to left)
 "%VSINSTALLDIR%\Team Tools\Dynamic Code Coverage Tools\CodeCoverage.exe" collect /output:"%CD%\\TestResults\VisualStudio.coverage" "%VSINSTALLDIR%\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe" "%VSINSTALLDIR%\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe" "/Logger:trx" "Implementation.UnitTests\bin\debug\Implementation.UnitTests.dll" "Crosscutting.Contracts.UnitTests\bin\debug\Crosscutting.Contracts.UnitTests.dll"
+REM Save coverage as xml
 "%VSINSTALLDIR%\Team Tools\Dynamic Code Coverage Tools\CodeCoverage.exe" analyze /output:"%CD%\\TestResults\VisualStudio.coveragexml" "%CD%\TestResults\VisualStudio.coverage"
 
-%ScannerLocation%\SonarQube.Scanner.MSBuild.exe end /d:sonar.login="e23d98e90e5f0bcea19b5278753638d6cdb83eab"
+REM Stop analyzing and generate/deploy report
+%ScannerLocation%\SonarQube.Scanner.MSBuild.exe end /d:sonar.login="de76097ef248e051959c31eb08f27898c306f311"
