6986e93d4ee393535425255742e9e113adc95f53
Eric Kerst <eric.kerst@gmail.com>
2018-03-15 00:21:07 +0100

    tuning

--- sonarcloud.cmd
+++ sonarcloud.cmd
@@ -1,7 +1,7 @@
 @ECHO ON
 REM Download SonarQube Scanner for MSBuild at https://docs.sonarqube.org/display/SCAN/Analyzing+with+SonarQube+Scanner+for+MSBuild 
 REM Change ScannerLocation to where SonarQube.Scanner.MSBuild.exe is located
-REM Open developer command prompt
+REM Open a visual studio developer command prompt!
 REM Change path to where SOLID.sln is located (eg. D:\Projects\SOLID)
 
 SET ScannerLocation=D:\SonarQube\Scanner
@@ -13,7 +13,7 @@ md "%CD%\TestResults"
 
 MsBuild.exe /t:Rebuild SOLID.sln
 
-"%VSINSTALLDIR%\Team Tools\Dynamic Code Coverage Tools\CodeCoverage.exe" collect /output:"%CD%\\TestResults\VisualStudio.coverage" "%VSINSTALLDIR%\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe" "%VSINSTALLDIR%\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe" /Logger:trx "Implementation.UnitTests\bin\debug\Implementation.UnitTests.dll" "Crosscutting.Contracts.UnitTests\bin\debug\Crosscutting.Contracts.UnitTests.dll"
+"%VSINSTALLDIR%\Team Tools\Dynamic Code Coverage Tools\CodeCoverage.exe" collect /output:"%CD%\\TestResults\VisualStudio.coverage" "%VSINSTALLDIR%\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe" "%VSINSTALLDIR%\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe" "/Logger:trx" "Implementation.UnitTests\bin\debug\Implementation.UnitTests.dll" "Crosscutting.Contracts.UnitTests\bin\debug\Crosscutting.Contracts.UnitTests.dll"
 "%VSINSTALLDIR%\Team Tools\Dynamic Code Coverage Tools\CodeCoverage.exe" analyze /output:"%CD%\\TestResults\VisualStudio.coveragexml" "%CD%\TestResults\VisualStudio.coverage"
 
 %ScannerLocation%\SonarQube.Scanner.MSBuild.exe end /d:sonar.login="e23d98e90e5f0bcea19b5278753638d6cdb83eab"
