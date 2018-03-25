397ce6c537c54b872ca9a441833acf480940354a
Eric Kerst <eric.kerst@gmail.com>
2018-03-25 18:52:49 +0200

    last changes

---
+++ src/netcore/Clients.WebApi.ConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Clients.WebApi.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new Client("http://localhost:51964/");
+            client.ApiCommandAddAdresPostAsync(new AddAdresCommand { Postcode = "2323" });
+            var result = client.ApiQueryAdresGetAsync("1", "2323");
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
