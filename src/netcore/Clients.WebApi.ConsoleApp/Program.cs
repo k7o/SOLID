1e540554505ecc2509f4a572266df3c0e131baf2
Eric Kerst <eric.kerst@gmail.com>
2018-03-30 01:11:12 +0200

    Tuned

--- src/netcore/Clients.WebApi.ConsoleApp/Program.cs
+++ src/netcore/Clients.WebApi.ConsoleApp/Program.cs
@@ -1,6 +1,6 @@
-﻿using Business.Contracts.Query.Zoek;
-using System;
-using System.Threading.Tasks;
+﻿using Business.Contracts.Command;
+using Business.Contracts.Query.Zoek;
+using System.Threading;
 
 namespace Clients.WebApi.ConsoleApp
 {
@@ -8,19 +8,20 @@ namespace Clients.WebApi.ConsoleApp
     {
         static void Main(string[] args)
         {
-            var cancellationToken = new System.Threading.CancellationToken();
+            var cancellationToken = new CancellationToken();
 
-            var commandClient = new CommandClient<Business.Contracts.Command.AddAdresCommand>();
-
+            var commandClient = new CommandClient<AddAdresCommand>();
             var queryClient = new QueryClient<AdresQuery, ZoekResult>();
 
-            var commandTask = commandClient.HandleAsync(new Business.Contracts.Command.AddAdresCommand("1111AA"), cancellationToken);
+            var commandHandleTask = commandClient.HandleAsync(new AddAdresCommand("1111AA"), cancellationToken);
 
-            var queryTask = queryClient.HandleAsync(new AdresQuery("1111AA"), cancellationToken);
+            var queryHandleTask = queryClient.HandleAsync(new AdresQuery("1111AA"), cancellationToken);
             
-            var result = queryTask.Result;
-            
-            commandTask.Wait();
+            var result = queryHandleTask.Result;
+            // what todo;)
+
+            commandHandleTask.Wait();
+            queryHandleTask.Wait();
         }
     }
 }
